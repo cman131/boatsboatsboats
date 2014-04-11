40539435de2c92fd5f80761138ccb698a21c4c22
cman131 <cgw5994@rit.edu>
2014-04-11 12:43:59 -0400

    added the table updating and mar boats to the boats boats boats that previously existed in boats boats boats making it into potentially boats boats boats boats or boats boats boats boats boats. I'm also considering boats boats boats boats * 7, but at that point it should be navy navy navy

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
+var score = 0;
+function update() {
+	document.getElementById("score").innerHTML = score+"/"+num;
+}
+
+function checkDatLyric(lyric){
+	return lyrics[lyric]!=null;
+}
+
+function strippinTime(words){
+	return words.replace(/\W/g, '');
+}
+
 function getParameterByName(name) {
     name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
     var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
         results = regex.exec(location.search);
     return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
+
